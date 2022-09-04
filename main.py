89808730fbe7bd5242cf2c2a739c2b85fe064325
dennismulder1978 <dnns.mldr@gmail.com>
2022-09-04 15:02:35 +0200

    first

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-'''
+"""
 Extract birthday dates and send them per mail
-'''
+"""
+import smtplib, ssl
+from Secret.const import *
+
+
+smtpobj = smtplib.SMTP(SMTP_Server, port)
+subject = "Verjaardag kalender komende verjaardagen"
+message = """
+Hello,
+Is it me your looking for?
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(SMTP_Server, port, context=context) as server:
+    server.login(mail_adress, mail_password)
+    server.sendmail(mail_adress, receiver_email, message)
