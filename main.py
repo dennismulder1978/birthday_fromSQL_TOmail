e7c92cdfb757439a37e35f13df5ea294a7393140
dennismulder1978 <dnns.mldr@gmail.com>
2022-09-04 15:14:12 +0200

    first

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 Extract birthday dates and send them per mail
 """
-import smtplib, ssl
+import smtplib
+import ssl
 from Secret.const import *
 
 
@@ -12,7 +13,8 @@ Hello,
 Is it me your looking for?
 """
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(SMTP_Server, port, context=context) as server:
+#context = ssl.create_default_context()
+with smtplib.SMTP(SMTP_Server, port) as server:
     server.login(mail_adress, mail_password)
     server.sendmail(mail_adress, receiver_email, message)
+    server.quit()
